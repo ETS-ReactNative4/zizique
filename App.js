f7bfde1a80e61d1030a036ddebaf0073fc82de69
llayton <lyes.souifi@gmail.com>
2022-02-18 10:16:51 +0100

    Ajout StackNavigation + début drawer

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { View } from 'react-native';
-import Home from "./src/screen/Home"
-import SelectScreen from './src/screen/SelectScreen';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import StackNavigation from './src/navigation/StackNavigation'
+
 export default function App() {
+
   return (
-    <View style={{ flex: 1}}>
-      <SelectScreen/>
-    </View>  
+      <NavigationContainer>
+        <StackNavigation/>    
+      </NavigationContainer>
+
   );
 }
 
