761c854b4d4957050452d9a5778ac33a49fde6aa
BeatlesBOB <beatlesbob74120@gmail.com>
2022-02-21 08:32:01 +0100

    Update RoomScreen.js

--- src/screen/RoomScreen.js
+++ src/screen/RoomScreen.js
@@ -115,12 +115,15 @@ class RoomScreen extends React.Component {
                         <View style={styles.histo_container}>
                             <View style={styles.histo_header}>
                                 <Text style={styles.title}>Historique</Text>
-                                <Victoire height={20} width={20} />
+                                <Victoire height={30} width={30} />
                             </View>
                             <HistoricList historique={this.state.historique}/>
                         </View>
-                        <View>
-
+                        <View style={styles.classement_container}>
+                            <View style={styles.classement_header}>
+                                <Text style={styles.title}>Classement</Text>
+                                <Victoire height={30} width={30} />
+                            </View>
                         </View>
                     </View>
                 </View>
@@ -164,6 +167,14 @@ const styles = StyleSheet.create({
         borderTopRightRadius:29,
         padding:5
     },
+    classement_container:{
+        flex:0.45,
+        marginTop:20,
+        backgroundColor:"rgba(0,0,0,.2)",
+        borderTopLeftRadius:29,
+        borderTopRightRadius:29,
+        padding:5
+    },
     histo_header:{
         flexDirection:"row",
         alignItems:"flex-end",
@@ -174,7 +185,15 @@ const styles = StyleSheet.create({
     title:{
         color:"white",
         fontSize:20
+    },
+    classement_container:{
+        flexDirection:"row",
+        alignItems:"flex-end",
+        marginBottom:5,
+        padding:10,
+        justifyContent:"space-between"
     }
+
 });
 
 
