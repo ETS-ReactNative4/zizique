510058c0214243285922b100082033679f847a0f
llayton <lyes.souifi@gmail.com>
2022-03-07 10:59:03 +0100

    Ajout store

--- App.js
+++ App.js
@@ -2,13 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigation from './src/navigation/StackNavigation'
+import { Provider } from 'mobx-react';
+import {storeConnexion} from './src/store/Store';
+import {configure} from 'mobx';
 
 export default function App() {
-
+  configure({
+    enforceActions:'never'
+  });
+  const stores = {storeConnexion}
   return (
-       <NavigationContainer>
-         <StackNavigation/>    
+    <Provider {...stores}>
+        <NavigationContainer>
+          <StackNavigation/>           
        </NavigationContainer>
+    </Provider>
+
       
 
   );
